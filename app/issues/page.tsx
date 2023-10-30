1ea2535b4bcf3d6ab3506120e88217be50c38694
Anil Puchakayala <anilpuchakayala@gmail.com>
2023-10-30 11:37:50 +0100

    Build a new Issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
-
 const IssuesPage = () => {
-  return (
-    <div>
-      <Button>New Issue</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default IssuesPage;
