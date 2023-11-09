909c0d529c9b861b34ce0cb76b03b571c1d3ff9b
Anil Puchakayala <anilpuchakayala@gmail.com>
2023-11-09 16:16:18 +0100

    build the issue summery component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import prisma from "@/prisma/client";
+import IssueSummery from "./IssueSummery";
 import LatestIssues from "./LatestIssues";
+import { Status } from "@prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: Status.OPEN } });
+  const closed = await prisma.issue.count({ where: { status: Status.CLOSED } });
+  const inProgress = await prisma.issue.count({
+    where: { status: Status.IN_PROGRESS },
+  });
+
   return (
     <>
+      <IssueSummery open={open} inProgress={inProgress} closed={closed} />
       <LatestIssues />
     </>
   );
