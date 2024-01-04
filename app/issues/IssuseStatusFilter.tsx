1da1d8a8eac64e93b1d38134cb0c8f22957975dd
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-04 16:18:02 +0100

    dropdown now updates correctly

--- app/issues/IssuseStatusFilter.tsx
+++ app/issues/IssuseStatusFilter.tsx
@@ -2,7 +2,7 @@
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -12,16 +12,23 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 const IssuseStatusFilter = () => {
   const router = useRouter();
-  const searchParms = useSearchParams();
+  const searchParams = useSearchParams();
+  const [selectedStatus, setSelectedStatus] = useState(searchParams.get("status") || "");
+
+  useEffect(() => {
+    setSelectedStatus(searchParams.get("status") || "");
+  }, [searchParams]);
+
   return (
     <Select.Root
-      defaultValue={searchParms.get("status") || ""}
+      value={selectedStatus}
       onValueChange={(status) => {
-        const parms = new URLSearchParams(searchParms);
+        setSelectedStatus(status);
+        const parms = new URLSearchParams(searchParams);
         parms.delete('status');
         if (status) parms.append("status", status);
-        if (searchParms.get("orderBy"))
-          parms.append("orderBy", searchParms.get("orderBy")!);
+        if (searchParams.get("orderBy"))
+          parms.append("orderBy", searchParams.get("orderBy")!);
         const query = parms.size ? "?" + parms.toString() : "";
         router.push(`/issues${query}`);
       }}
