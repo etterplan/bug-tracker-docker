fccd116894cad17ad413e1e683f019e059d2ae92
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-15 14:20:28 +0100

    Added comments id route and delete function to api

---
+++ app/api/comments/[id]/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/prisma/client";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session)
+    return NextResponse.json({}, { status: 401 });
+
+  const comment = await prisma.comment.findUnique({ where: { id: parseInt(params.id) } });
+
+  if (!comment) return NextResponse.json({ error: "Invalid comment" }, { status: 404 });
+
+  await prisma.comment.delete({ where: { id: comment.id } })
+  return NextResponse.json({})
+
+}
