4120b43af1665a7405fbc77dc05e4d5f09eaf470
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-16 16:14:36 +0100

    Added patch function to comments/id route

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -17,3 +17,22 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: s
   return NextResponse.json({})
 
 }
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session)
+    return NextResponse.json({}, { status: 401 });
+
+  const comment = await prisma.comment.findUnique({ where: { id: parseInt(params.id) } });
+
+  if (!comment) return NextResponse.json({ error: "Invalid comment" }, { status: 404 });
+
+  const { text } = await request.json();
+
+  await prisma.comment.update({
+    where: { id: comment.id },
+    data: { text: text }
+  });
+
+  return NextResponse.json({});
+}
