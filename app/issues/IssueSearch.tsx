d5609d1b3a325e6c01b801968dd4c5cab54dc890
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-22 13:23:30 +0100

    created search component and functionality

--- app/issues/IssueSearch.tsx
+++ app/issues/IssueSearch.tsx
@@ -1,9 +1,35 @@
-
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from 'react';
+import { TextField } from "@radix-ui/themes";
+import { MagnifyingGlassIcon} from "@radix-ui/react-icons";
 
 const IssueSearch = () => {
-  return (
-    <div>IssueSearch</div>
-  )
-}
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [searchText, setSearchText] = useState(searchParams.get('search') || '');
 
-export default IssueSearch
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    params.delete('search');
+    if (searchText && searchText !== '') params.append('search', searchText);
+    const query = params.toString() ? '?' + params.toString() : '';
+    router.push(`/issues${query}`);
+  }, [searchText]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <TextField.Root>
+      <TextField.Slot>
+      <MagnifyingGlassIcon height="16" width="16" />
+      </TextField.Slot>
+      <TextField.Input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Search issues..."
+      />
+    </TextField.Root>
+  );
+};
+
+export default IssueSearch;
