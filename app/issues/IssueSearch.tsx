9fb4e7db75a37584e8e9115c9cbd5bc4dc9df195
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-23 11:43:02 +0100

    debounced component

--- app/issues/IssueSearch.tsx
+++ app/issues/IssueSearch.tsx
@@ -1,26 +1,31 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { TextField } from "@radix-ui/themes";
-import { MagnifyingGlassIcon} from "@radix-ui/react-icons";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { debounce } from 'lodash';
 
 const IssueSearch = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [searchText, setSearchText] = useState(searchParams.get('search') || '');
 
-  useEffect(() => {
+  const debouncedSearch = useRef(debounce((text) => {
     const params = new URLSearchParams(searchParams);
     params.delete('search');
-    if (searchText && searchText !== '') params.append('search', searchText);
+    if (text && text !== '') params.append('search', text);
     const query = params.toString() ? '?' + params.toString() : '';
     router.push(`/issues${query}`);
+  }, 750)).current;
+
+  useEffect(() => {
+    debouncedSearch(searchText);
   }, [searchText]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <TextField.Root>
       <TextField.Slot>
-      <MagnifyingGlassIcon height="16" width="16" />
+        <MagnifyingGlassIcon height="16" width="16" />
       </TextField.Slot>
       <TextField.Input
         type="text"
