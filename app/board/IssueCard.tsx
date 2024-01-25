36d44da3d071e8d1d9b5ef390f29b25fac4d6f6d
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-25 14:00:50 +0100

    added assigned user image to issuecard

--- app/board/IssueCard.tsx
+++ app/board/IssueCard.tsx
@@ -1,8 +1,13 @@
 'use client'
 import { Flex, Card, Avatar, Text, Grid } from "@radix-ui/themes";
-import { Issue } from "@prisma/client";
+import { Issue as PrismaIssue, User } from "@prisma/client";
 import { PriorityIcon } from "@/app/components";
 import { PersonIcon } from "@radix-ui/react-icons";
+
+type Issue = PrismaIssue & {
+  assignedToUser: User | null;
+};
+
 const IssueCard = ({ issues }: { issues: Issue[] }) => {
   return (
     <Grid gap="1">
@@ -17,9 +22,9 @@ const IssueCard = ({ issues }: { issues: Issue[] }) => {
             </Flex>
             <Avatar
               size="3"
-              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop" 
+              src={issue.assignedToUser?.image ?? undefined}
               radius="full"
-              fallback={<PersonIcon width="32" height="32" />}
+              fallback={<PersonIcon width="28" height="28" />}
               />
           </Flex>
         </Card>
