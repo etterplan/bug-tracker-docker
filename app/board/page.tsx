525675530ac1124a3e52e7ea0bafb0c9d07fcbbb
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-25 13:59:53 +0100

    included user in issues call

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,20 +1,30 @@
-import { Flex, Grid } from "@radix-ui/themes";
+import { Flex, Grid, Box } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import { Metadata } from "next";
 import IssueCard from "./IssueCard";
 
 export default async function Board() {
-  const issues = await prisma.issue.findMany({})
+  const issues = await prisma.issue.findMany({
+    include: {
+      assignedToUser: true,
+    },
+  })
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Flex>
+    <Grid columns={{ initial: "1", sm: "3" }} gap="5">
+      <Box>
         <IssueCard issues={issues} />
-      </Flex>
+      </Box>
+      <Box>
+        <IssueCard issues={issues} />
+      </Box>
+      <Box>
+        <IssueCard issues={issues} />
+      </Box>
     </Grid>
   )
 }
 
 export const metadata: Metadata = {
-  title: "Issue Tracker - Board",
+  title: "Issue Tracker - Issue Board",
   description: "View a summary of project issue",
 };
