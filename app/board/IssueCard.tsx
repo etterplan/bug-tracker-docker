0231e04ced3aa4733eb99bb00f0c022a93e0b8e6
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-26 12:25:26 +0100

    made component into a simple card component

--- app/board/IssueCard.tsx
+++ app/board/IssueCard.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Flex, Card, Avatar, Text, Grid } from "@radix-ui/themes";
 import { Issue as PrismaIssue, User } from "@prisma/client";
 import { PriorityIcon } from "@/app/components";
@@ -8,27 +7,25 @@ type Issue = PrismaIssue & {
   assignedToUser: User | null;
 };
 
-const IssueCard = ({ issues }: { issues: Issue[] }) => {
+const IssueCard = ({ issue }: { issue: Issue }) => {
   return (
     <Grid gap="1">
-      {issues.map((issue) => (
-        <Card key={issue.id}>
-          <Flex gap="3" justify="between" align="center">
-            <Flex direction="column" align="start" gap="2">
-              <Text as="div" size="2" weight="medium">
-                {issue.title}
-              </Text>
-              <PriorityIcon priority={issue.priority} />
-            </Flex>
-            <Avatar
-              size="3"
-              src={issue.assignedToUser?.image ?? undefined}
-              radius="full"
-              fallback={<PersonIcon width="28" height="28" />}
-              />
+      <Card key={issue.id}>
+        <Flex gap="3" justify="between" align="center">
+          <Flex direction="column" align="start" gap="2">
+            <Text as="div" size="2" weight="medium">
+              {issue.title}
+            </Text>
+            <PriorityIcon priority={issue.priority} />
           </Flex>
-        </Card>
-      ))}
+          <Avatar
+            size="3"
+            src={issue.assignedToUser?.image ?? undefined}
+            radius="full"
+            fallback={<PersonIcon width="28" height="28" />}
+          />
+        </Flex>
+      </Card>
     </Grid>
   )
 }
