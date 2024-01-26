ceab7af307009cf4f0f32350b1eb19051c00ef93
Sebastian Locksenius <slocksenius@gmail.com>
2024-01-26 12:23:57 +0100

    Filtered issues based on status and sent them to new component

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,26 +1,37 @@
 import { Flex, Grid, Box } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import { Metadata } from "next";
-import IssueCard from "./IssueCard";
+import BoardLayout from "./BoardLayout";
 
 export default async function Board() {
-  const issues = await prisma.issue.findMany({
+  const openIssues = await prisma.issue.findMany({
+    where: {
+      status: 'OPEN'
+    },
     include: {
       assignedToUser: true,
     },
-  })
+  });
+
+  const inProgressIssues = await prisma.issue.findMany({
+    where: {
+      status: 'IN_PROGRESS'
+    },
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const closedIssues = await prisma.issue.findMany({
+    where: {
+      status: 'CLOSED'
+    },
+    include: {
+      assignedToUser: true,
+    },
+  });
   return (
-    <Grid columns={{ initial: "1", sm: "3" }} gap="5">
-      <Box>
-        <IssueCard issues={issues} />
-      </Box>
-      <Box>
-        <IssueCard issues={issues} />
-      </Box>
-      <Box>
-        <IssueCard issues={issues} />
-      </Box>
-    </Grid>
+    <BoardLayout openIssues={openIssues} inProgressIssues={inProgressIssues} closedIssues={closedIssues} />
   )
 }
 
