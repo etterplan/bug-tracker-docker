92b2ed26993d0f079778f265df40a5ad6348cd32
Sebastian Locksenius <slocksenius@gmail.com>
2024-02-09 16:31:55 +0100

    made issuecard a simple card component

--- app/board/IssueCard.tsx
+++ app/board/IssueCard.tsx
@@ -9,32 +9,27 @@ type Issue = PrismaIssue & {
 };
 
 const IssueCard = ({ issue }: { issue: Issue }) => {
-  const onDragStart = (e: React.DragEvent) => {
-    e.dataTransfer.setData("text/plain", issue.id.toString());
-    (e.target as HTMLElement).style.opacity = "0.4";
-  };
 
   return (
-    <div draggable onDragStart={onDragStart}>
-      <Grid className="mb-1">
-        <Card key={issue.id}>
-          <Flex gap="3" justify="between" align="center">
-            <Flex direction="column" align="start" gap="2">
-              <Text as="div" size="2" weight="medium">
-                {issue.title}
-              </Text>
-              <PriorityIcon priority={issue.priority} id={issue.id} />
-            </Flex>
-            <Avatar
-              size="3"
-              src={issue.assignedToUser?.image ?? undefined}
-              radius="full"
-              fallback={<PersonIcon width="28" height="28" />}
-            />
+    <Grid className="mb-1">
+      <Card key={issue.id}>
+        <Flex gap="3" justify="between" align="center">
+          <Flex direction="column" align="start" gap="2">
+            <Text as="div" size="2" weight="medium">
+              {issue.title}
+            </Text>
+            <PriorityIcon priority={issue.priority} id={issue.id} />
           </Flex>
-        </Card>
-      </Grid>
-    </div>
+          <Avatar
+            size="3"
+            src={issue.assignedToUser?.image ?? undefined}
+            radius="full"
+            fallback={<PersonIcon width="28" height="28" />}
+          />
+        </Flex>
+      </Card>
+    </Grid>
+
   );
 };
 
