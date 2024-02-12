6421abf5315ec348e47c820c2388c4d92774e90a
anilpuchakayala <anilpuchakayala@gmail.com>
2024-02-12 12:18:48 +0100

    Added project component and route for the same

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,18 @@
+import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
+import ProjectAction from "./ProjectAction";
 
 const ProjectsPage = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <Flex direction="column" gap="3">
+      <ProjectAction />
+    </Flex>
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Projects List",
+  description: "View a list of projects",
+};
