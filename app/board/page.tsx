fd5df6deff1f8d33e0f7521cb7a2325d26ad9448
Sebastian Locksenius <slocksenius@gmail.com>
2024-02-13 17:22:57 +0100

    added filtering of issues by boardPosition

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,9 +1,16 @@
-import { Flex, Grid, Box } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import { Metadata } from "next";
 import BoardLayout from "./BoardLayout";
+import { Prisma } from "@prisma/client";
 
 export const dynamic = "force-dynamic";
+
+type Issue = Prisma.IssueGetPayload<{
+  include: {
+    assignedToUser: true;
+  }
+}>;
 
 export default async function Board() {
   const openIssues = await prisma.issue.findMany({
@@ -13,6 +20,9 @@ export default async function Board() {
     include: {
       assignedToUser: true,
     },
+    orderBy: [
+      { id: 'asc' },
+    ],
   });
 
   const inProgressIssues = await prisma.issue.findMany({
@@ -22,6 +32,9 @@ export default async function Board() {
     include: {
       assignedToUser: true,
     },
+    orderBy: [
+      { id: 'asc' },
+    ],
   });
 
   const closedIssues = await prisma.issue.findMany({
@@ -31,13 +44,40 @@ export default async function Board() {
     include: {
       assignedToUser: true,
     },
+    orderBy: [
+      { id: 'asc' },
+    ],
   });
+
+  const rearrangeIssues = (issues: Issue[]): Issue[] => {
+    const sortedIssues: (Issue | null)[] = new Array(issues.length).fill(null);
+
+    issues.forEach((issue) => {
+      if (issue.boardPosition !== null && issue.boardPosition < issues.length) {
+        sortedIssues[issue.boardPosition] = issue;
+      }
+    });
+
+    issues.forEach((issue) => {
+      if (issue.boardPosition === null || issue.boardPosition >= issues.length) {
+        const nullIndex = sortedIssues.indexOf(null);
+        if (nullIndex !== -1) {
+          sortedIssues[nullIndex] = issue;
+        } else {
+          sortedIssues.push(issue);
+        }
+      }
+    });
+
+    return sortedIssues.filter(issue => issue !== null) as Issue[];
+  };
+
   return (
     <Grid columns={{ initial: "1", sm: "3" }} gap="5">
       <BoardLayout
-        openIssues={openIssues}
-        inProgressIssues={inProgressIssues}
-        closedIssues={closedIssues}
+        openIssues={rearrangeIssues(openIssues)}
+        inProgressIssues={rearrangeIssues(inProgressIssues)}
+        closedIssues={rearrangeIssues(closedIssues)}
       />
     </Grid>
   );
