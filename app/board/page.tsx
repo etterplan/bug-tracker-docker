ed0186272d4fe8d5f2bd0d656f82e0d51f3c13a9
Sebastian Locksenius <slocksenius@gmail.com>
2024-02-22 11:02:22 +0100

    fixed typescript warning

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -4,8 +4,6 @@ import { Grid } from "@radix-ui/themes";
 import { Metadata } from "next";
 import ShowBoard from "./ShowBoard";
 
-// export const dynamic = "force-dynamic";
-
 type Issue = Prisma.IssueGetPayload<{
   include: {
     assignedToUser: true;
@@ -13,69 +11,6 @@ type Issue = Prisma.IssueGetPayload<{
 }>;
 
 export default async function Board() {
-  /* const openIssues = await prisma.issue.findMany({
-    where: {
-      status: "OPEN",
-    },
-    include: {
-      assignedToUser: true,
-    },
-    orderBy: [{ id: "asc" }],
-  });
-
-  const inProgressIssues = await prisma.issue.findMany({
-    where: {
-      status: "IN_PROGRESS",
-    },
-    include: {
-      assignedToUser: true,
-    },
-    orderBy: [{ id: "asc" }],
-  });
-
-  const closedIssues = await prisma.issue.findMany({
-    where: {
-      status: "CLOSED",
-    },
-    include: {
-      assignedToUser: true,
-    },
-    orderBy: [{ id: "asc" }],
-  });
-
-  const rearrangeIssues = (issues: Issue[]): Issue[] => {
-    const sortedIssues: (Issue | null)[] = new Array(issues.length).fill(null);
-
-    issues.forEach((issue) => {
-      if (issue.position !== null && issue.position < issues.length) {
-        sortedIssues[issue.position] = issue;
-      }
-    });
-
-    issues.forEach((issue) => {
-      if (issue.position === null || issue.position >= issues.length) {
-        const nullIndex = sortedIssues.indexOf(null);
-        if (nullIndex !== -1) {
-          sortedIssues[nullIndex] = issue;
-        } else {
-          sortedIssues.push(issue);
-        }
-      }
-    });
-
-    return sortedIssues.filter((issue) => issue !== null) as Issue[];
-  }; 
-
-    return (
-    <Grid columns={{ initial: "1", sm: "3" }} gap="5">
-      <BoardLayout
-        openIssues={rearrangeIssues(openIssues)}
-        inProgressIssues={rearrangeIssues(inProgressIssues)}
-        closedIssues={rearrangeIssues(closedIssues)}
-      />
-    </Grid>
-  );
-*/
   const issuesByStatus: Record<Status, Issue[]> = {
     [Status.TODO]: [],
     [Status.OPEN]: [],
@@ -86,6 +21,9 @@ export default async function Board() {
   const boardIssues = await prisma.issue.findMany({
     where: {
       boardId: 1,
+    },
+    include: {
+      assignedToUser: true,
     },
   });
   boardIssues.forEach((issue) => {
