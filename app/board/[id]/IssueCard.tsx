916b789699a6f34625e729c9021de482a5f6ce4f
Sebastian Locksenius <slocksenius@gmail.com>
2024-02-26 18:50:31 +0100

    minor styling

--- app/board/[id]/IssueCard.tsx
+++ app/board/[id]/IssueCard.tsx
@@ -10,24 +10,22 @@ type Issue = PrismaIssue & {
 
 const IssueCard = ({ issue }: { issue: Issue }) => {
   return (
-    <Grid className="mb-1">
-      <Card key={issue.id}>
-        <Flex gap="3" justify="between" align="center">
-          <Flex direction="column" align="start" gap="2">
-            <Text as="div" size="2" weight="medium">
-              {issue.title}
-            </Text>
-            <PriorityIcon priority={issue.priority} id={issue.id} />
-          </Flex>
-          <Avatar
-            size="3"
-            src={issue.assignedToUser?.image ?? undefined}
-            radius="full"
-            fallback={<PersonIcon width="28" height="28" />}
-          />
+    <Card key={issue.id} className="m-3">
+      <Flex gap="3" justify="between" align="center">
+        <Flex direction="column" align="start" gap="2">
+          <Text as="div" size="2" weight="medium">
+            {issue.title}
+          </Text>
+          <PriorityIcon priority={issue.priority} id={issue.id} />
         </Flex>
-      </Card>
-    </Grid>
+        <Avatar
+          size="3"
+          src={issue.assignedToUser?.image ?? undefined}
+          radius="full"
+          fallback={<PersonIcon width="28" height="28" />}
+        />
+      </Flex>
+    </Card>
   );
 };
 
