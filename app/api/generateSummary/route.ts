c102ab61821cd56072b2f9bca639045fbbbf8b41
anilpuchakayala <anilpuchakayala@gmail.com>
2024-03-14 15:08:21 +0100

    Added chatGPT summery to the dashboard

--- app/api/generateSummary/route.ts
+++ app/api/generateSummary/route.ts
@@ -13,14 +13,23 @@ export async function POST(request: NextRequest) {
     const {tasks} = await request.json();
 
   // communicate with chat GPT
+  const userName = session && session.user && session.user.name ? session.user.name : "User";
+  const prompt = `Hi there! Please provide a summary of the following tasks. Count how many tasks are in each category such as Todo, open, in progress, and closed. Then, tell the user to have a productive day! Here's the data: ${JSON.stringify(tasks)}`;
 
-    const prompt = `Hi there, provide a summary of the following tasks. Count how many tasks are in each category such as Todo, in progress, done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(tasks)}`;
-    const chatCompletion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      n:1,
-      stream:false,
-      messages: [{"role": "system", "content": `When responding, welcome the user always as ${session.user?.name} and say welcome to the tracker App! Limit the response to 200 characters`},
-      {"role": "user", "content": prompt}],
-    });
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        "role": "system",
+        "content": `When responding, always welcome the user as ${userName} and say, "Welcome to the bug tracker app!" Limit the response to 200 characters.`
+      },
+      {
+        "role": "user",
+        "content": prompt
+      }
+    ]
+  });
     return NextResponse.json(chatCompletion.choices[0].message, { status: 201 });
   }
