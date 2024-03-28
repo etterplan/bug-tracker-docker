4dfbfa9d08eebfe7d38008c1909de0f0751ed489
Sebastian Locksenius <slocksenius@gmail.com>
2024-03-28 17:52:29 +0100

    started on new editing of project description

--- app/projects/[id]/ProjectInfo.tsx
+++ app/projects/[id]/ProjectInfo.tsx
@@ -1,14 +1,39 @@
 'use client'
-import { Card, Flex, Heading, Text, Box } from '@radix-ui/themes';
+import { Card, Flex, Heading, Text, Box, Button, TextArea } from '@radix-ui/themes';
 import ProjectStatusBadge from '../../components/ProjectStatusBadge'
 import { Project } from "@prisma/client";
-import EditProjectButton from './EditProjectButton';
+import { useState } from 'react'
+import axios from "axios";
 
 interface Props {
   project: Project;
 }
 
 const ProjectInfo = ({ project }: Props) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [description, setDescription] = useState(project.description)
+  const [error, setError] = useState('');
+
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value)
+  }
+
+  const handleSaveClick = async () => {
+    try {
+
+      await axios.patch("/api/projects/" + project.id, { description: description });
+
+
+    } catch (error) {
+      setError((error as Error).message);
+    }
+    setIsEditing(false)
+  }
+
   return (
     <Box className="md:col-span-4">
       <Heading>{project.name}</Heading>
@@ -20,9 +45,22 @@ const ProjectInfo = ({ project }: Props) => {
         <Text>Updated: {project.updatedAt.toDateString()}</Text>
       </Flex>
       <Card className="prose max-w-full" my="1">
-        <Text>{project.description}</Text>
+        {isEditing ? (
+          <Flex direction='column'>
+            <TextArea value={description} onChange={handleDescriptionChange} />
+            <Flex justify='end'>
+              <Button onClick={handleSaveClick}>Save</Button>
+            </Flex>
+          </Flex>
+        ) : (
+          <Flex direction='column'>
+            <Text>{description}</Text>
+            <Flex justify='end'>
+              <Button onClick={handleEditClick}>Edit</Button>
+            </Flex>
+          </Flex>
+        )}
       </Card>
-      <EditProjectButton projectId={project.id}/>
     </Box>
   )
 }
