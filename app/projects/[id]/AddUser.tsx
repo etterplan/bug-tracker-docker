9e268c431c002f991cf45e33f7d9b75296be0e37
Sebastian Locksenius <slocksenius@gmail.com>
2024-04-05 11:55:08 +0200

    made into parent component of new UserComboBox

--- app/projects/[id]/AddUser.tsx
+++ app/projects/[id]/AddUser.tsx
@@ -1,97 +1,21 @@
 'use client'
-import { useState, useMemo, startTransition } from 'react';
-import {  Flex, Button } from '@radix-ui/themes';
-import { Combobox, ComboboxItem, ComboboxList, ComboboxProvider } from "@ariakit/react";
-import * as RadixSelect from "@radix-ui/react-select";
-import { matchSorter } from 'match-sorter'
-import { MagnifyingGlassIcon, ChevronDownIcon, CheckIcon } from '@radix-ui/react-icons';
-import useUser from '../../components/useUser';
+import UserComboBox from "@/app/components/UserComboBox"
+import { useState } from 'react';
 
 const AddUser = () => {
-  const { data: users, error, isLoading } = useUser();
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
-  const [searchValue, setSearchValue] = useState('');
+  const [selectedUserId, setSelectedUserId] = useState('');
 
-  const matches = useMemo(() => {
-    if (!users) return [];
-    if (!searchValue) return users;
-    const keys = ["name", "id"];
-    const matches = matchSorter(users, searchValue, { keys });
-    const selectedUser = users.find((user) => user.id === value);
-    if (selectedUser && !matches.includes(selectedUser)) {
-      matches.push(selectedUser);
-    }
-    return matches;
-  }, [searchValue, value, users]);
+  const handleUserChange = (userId: string) => {
+    setSelectedUserId(userId);
+  };
 
   return (
-    <RadixSelect.Root
-      value={value}
-      onValueChange={setValue}
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <ComboboxProvider
-        open={open}
-        setOpen={setOpen}
-        resetValueOnHide
-        includesBaseElement={false}
-        setValue={(value) => {
-          startTransition(() => {
-            setSearchValue(value);
-          });
-        }}
-      >
-        <RadixSelect.Trigger aria-label="User" className="select" >
-          <RadixSelect.Value placeholder="Select a User" />
-          <RadixSelect.Icon className="select-icon">
-            <ChevronDownIcon />
-          </RadixSelect.Icon>
-        </RadixSelect.Trigger>
-        <RadixSelect.Content
-          role="dialog"
-          aria-label="Users"
-          position="popper"
-          className="popover"
-          sideOffset={4}
-          alignOffset={-16}
-        >
-          <Flex className="combobox-wrapper">
-            <Flex className="combobox-icon">
-              <MagnifyingGlassIcon />
-            </Flex>
-            <Combobox
-              autoSelect
-              placeholder="Search Users"
-              className="combobox"
-              onBlurCapture={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-              }}
-            />
-          </Flex>
-          <ComboboxList className="listbox">
-            {matches.map(({ name, id }) => (
-              <RadixSelect.Item
-                key={id}
-                value={id}
-                asChild
-                className="item"
-              >
-                <ComboboxItem>
-                  <RadixSelect.ItemText>{name}</RadixSelect.ItemText>
-                  <RadixSelect.ItemIndicator className="item-indicator">
-                    <CheckIcon />
-                  </RadixSelect.ItemIndicator>
-                </ComboboxItem>
-              </RadixSelect.Item>
-            ))}
-          </ComboboxList>
-        </RadixSelect.Content>
-      </ComboboxProvider>
-    </RadixSelect.Root>
-  );
+    <div>
+      <h1>Select a User</h1>
+      <UserComboBox onValueChange={handleUserChange} />
+      {selectedUserId && <p>Selected User ID: {selectedUserId}</p>}
+    </div>
+  )
 }
 
 export default AddUser
