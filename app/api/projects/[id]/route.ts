3dacb2cd49d00d7d183dbb97eeec5c2a95c0b456
Sebastian Locksenius <slocksenius@gmail.com>
2024-04-09 12:12:16 +0200

    updated api to add user to project

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -7,21 +7,38 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
 
-    const session = await getServerSession(authOptions);
-    if (!session) return NextResponse.json({}, { status: 401 });
-    const body = await request.json();
-    
-    const validation = updateProjectSchema.safeParse(body);
-    if (!validation.success)
-        return NextResponse.json(validation.error.format(), { status: 400 })
-    const {  name, description, status } = body;
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+  const body = await request.json();
 
-    const project = await prisma.project.findUnique({ where: { id: parseInt(params.id) } })
-
-    if (!project)
-        return NextResponse.json({ error: "Invalid project" }, { status: 404 })
-
-    const updateProject = await prisma.project.update({ where: { id: parseInt(params.id) }, data: { name: name, description: description, status: status } })
-    return NextResponse.json(updateProject)
-  
+  const validation = updateProjectSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 })
+  const { name, description, status, userId } = body;
+  let user
+  if (userId) {
+    user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 })
   }
+
+  const project = await prisma.project.findUnique({ where: { id: parseInt(params.id) } })
+
+  if (!project)
+    return NextResponse.json({ error: "Invalid project" }, { status: 404 })
+
+  const updateProject = await prisma.project.update({
+    where: { id: parseInt(params.id) },
+    data: {
+      name: name,
+      description: description,
+      status: status,
+      members: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  })
+  return NextResponse.json(updateProject)
+}
