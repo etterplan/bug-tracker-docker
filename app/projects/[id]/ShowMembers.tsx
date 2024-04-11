c86c28e57f6b807a553d52a945c993f21b9b33c2
Sebastian Locksenius <slocksenius@gmail.com>
2024-04-11 15:20:58 +0200

    added showing project members

---
+++ app/projects/[id]/ShowMembers.tsx
@@ -0,0 +1,40 @@
+import { User } from "@prisma/client";
+import { Flex, Text, Strong, Avatar, Card, Box } from "@radix-ui/themes";
+import { PersonIcon } from "@radix-ui/react-icons";
+
+interface Props {
+  members?: User[];
+}
+
+const ShowMembers = ({ members = [] }: Props) => {
+
+  return (
+    <Flex direction='column'mt='3'>
+      <Text weight='medium'>Project Members</Text>
+      <Flex my='2' gap='3'>
+        {members.map((member) => (
+          <Card key={member.id}>
+            <Flex align='center' gap='3'>
+              <Avatar
+                size='3'
+                src={member.image || undefined}
+                fallback={<PersonIcon width='36' height='36' />}
+                radius='full'
+              />
+              <Flex direction='column'>
+                <Text size='2' align='center'>
+                  <Strong>{member.name}</Strong>
+                </Text>
+                <Text size='1' align='center'>
+                  {member.email}
+                </Text>
+              </Flex>
+            </Flex>
+          </Card>
+        ))}
+      </Flex>
+    </Flex>
+  )
+}
+
+export default ShowMembers
